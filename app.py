13de6b8d91d2b66c546623e2a870547203a3dd53
Beanzix <30658931+Beanzix@users.noreply.github.com>
2017-08-30 17:51:01 +0800

    Update app.py

--- app.py
+++ app.py
@@ -28,31 +28,27 @@ def webhook():
     return r
 
 def makeWebhookResult(req):
-    if req.get("result").get("action") != "shipping.cost":
-        return {}
-    result = req.get("result")
-    parameters = result.get("parameters")
-    zone = parameters.get("shipping-zone")
-
-    cost = {'Europe':100, 'North America':200, 'South America':300, 'Asia':400, 'Africa':500}
-
-    speech = "The cost of shipping to " + zone + " is " + str(cost[zone]) + " euros."
-
+    if req.get("result").get("action") == "shipping.cost":           
+        result = req.get("result")
+        parameters = result.get("parameters")
+        zone = parameters.get("shipping-zone")
+        cost = {'Europe':100, 'North America':200, 'South America':300, 'Asia':400, 'Africa':500}
+        speech = "The cost of shipping to " + zone + " is " + str(cost[zone]) + " euros."
+        
     # wx add below
-    if req.get("result").get("action") != "return.policy-choice": #intent
-        return {}
-    result = req.get("result")
-    parameters = result.get("parameters")
-    zone = parameters.get("return-choice")
-
-    choice-policy = {
-                    'online returns':'please label your online ID and order number in the package you are returning. ',
-                    'gift returns':'gift refunds will be issued as an eGift Card within 24 hours of us receiving your returned item(s).',
-                    'returns in-store':'please bring your original receipt and return the product in any Orange retail store.'
-                    }
-
-    speech = "The policy to " + zone + " is " + str(choice-policy[zone])
+    elif req.get("result").get("action") == "return.policy-choice": #intent        
+        result = req.get("result")
+        parameters = result.get("parameters")
+        zone = parameters.get("return-choice")
+        choice-policy = {
+                        'online returns':'please label your online ID and order number in the package you are returning. ',
+                        'gift returns':'gift refunds will be issued as an eGift Card within 24 hours of us receiving your returned item(s).',
+                        'returns in-store':'please bring your original receipt and return the product in any Orange retail store.'
+                        }
+        speech = "The policy to " + zone + " is " + str(choice-policy[zone])
 #wx add above
+    else:
+        return {}
     
     print("Response:")
     print(speech)
