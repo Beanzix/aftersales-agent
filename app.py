6810cb14d6cba7d7d94e0db6cf44271fc6e65b8b
Beanzix <30658931+Beanzix@users.noreply.github.com>
2017-08-30 17:32:54 +0800

    Update app.py

--- app.py
+++ app.py
@@ -38,6 +38,22 @@ def makeWebhookResult(req):
 
     speech = "The cost of shipping to " + zone + " is " + str(cost[zone]) + " euros."
 
+    # wx add below
+    if req.get("result").get("action") != "return.policy-choice": #intent
+        return {}
+    result = req.get("result")
+    parameters = result.get("parameters")
+    zone = parameters.get("return-choice")
+
+    choice-policy = {
+                    'online returns':'please label your online ID and order number in the package you are returning. ',
+                    'gift returns':'gift refunds will be issued as an eGift Card within 24 hours of us receiving your returned item(s).',
+                    'returns in-store':'please bring your original receipt and return the product in any Orange retail store.'
+                    }
+
+    speech = "The policy to " + zone + " is " + str(choice-policy[zone])
+#wx add above
+    
     print("Response:")
     print(speech)
 
